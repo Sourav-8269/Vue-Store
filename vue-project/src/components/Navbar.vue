09009c38937e35a4efa1292a0b46c1d684265c94
Sourav <souravsahu70047@gmail.com>
2023-05-17 18:25:12 +0530

    Added Navbar

---
+++ vue-project/src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div id="navbar">
+        <div id="left">
+            <h1>Home</h1>
+        </div>
+        <div id="right">
+            <div>Account</div>
+            <div id="cart">
+                <div id="cart_count">1</div>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script scoped></script>
+
+<style scoped>
+#navbar{
+    width:90%;
+    padding: 1%;
+    margin: auto;
+    display: flex;
+    height: 70px;
+    align-items: center;
+    border-bottom: 1px solid black;
+}
+#left{
+    display: flex;
+    flex: 3;
+    align-items: center;
+}
+#right{
+    display: flex;
+    flex: 7;
+    align-items: center;
+    justify-content: flex-end;
+    gap: 10px;
+}
+</style>
+
