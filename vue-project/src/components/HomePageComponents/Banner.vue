0d7acd31c986f53670464a40563a038d38dac832
Sourav <souravsahu70047@gmail.com>
2023-05-18 16:54:24 +0530

    Added Qusar

--- vue-project/src/components/HomePageComponents/Banner.vue
+++ vue-project/src/components/HomePageComponents/Banner.vue
@@ -11,7 +11,6 @@ defineProps(["welcomemsg"]);
 #welcome_div{
   width: 90%;
   margin: auto;
-  padding: 1%;
   color: #0088cc;
   display: flex;
   justify-content: center;
@@ -19,6 +18,7 @@ defineProps(["welcomemsg"]);
   border-bottom: 1px solid black;
 }
 h1{
+  font-size: 40px;
   font-weight: 500;
   font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
 }
--- vue-project/src/components/HomePageComponents/Navbar.vue
+++ vue-project/src/components/HomePageComponents/Navbar.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
     <div id="navbar">
-        <div id="left">
-            <h1>Home</h1>
+      <div id="left">
+        <h1>Home</h1>
+      </div>
+      <div id="right">
+        <div>Account:</div>
+        <div id="cart">
+            <q-icon name="shopping_cart" ></q-icon>
+          <div id="cart_count">1</div>
         </div>
-        <div id="right">
-            <div>Account:</div>
-            <div id="cart">
-                <div id="cart_count">1</div>
-            </div>
-        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -17,30 +18,37 @@
 <script scoped></script>
 
 <style scoped>
-h1{
+h1 {
+  font-size: 40px;
   font-weight: 500;
-  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
 }
-#navbar{
-    width:90%;
-    padding: 1%;
-    margin: auto;
-    display: flex;
-    height: 70px;
-    align-items: center;
-    border-bottom: 1px solid black;
+#navbar {
+  width: 90%;
+  padding: 1%;
+  margin: auto;
+  display: flex;
+  height: 70px;
+  align-items: center;
+  border-bottom: 1px solid black;
 }
-#left{
+#left {
+  display: flex;
+  flex: 3;
+  align-items: center;
+}
+#right {
+  display: flex;
+  flex: 7;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 10px;
+}
+#cart{
     display: flex;
-    flex: 3;
     align-items: center;
 }
-#right{
-    display: flex;
-    flex: 7;
-    align-items: center;
-    justify-content: flex-end;
-    gap: 10px;
+#cart_count{
+    padding-bottom: 12px;
 }
 </style>
-
