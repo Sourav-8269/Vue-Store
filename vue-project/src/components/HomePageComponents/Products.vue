06b71c24f4a4c5e27943624cac0589ac9dea6abd
Sourav <souravsahu70047@gmail.com>
2023-05-19 22:26:43 +0530

    Added Data in Products Page

--- vue-project/src/components/HomePageComponents/Products.vue
+++ vue-project/src/components/HomePageComponents/Products.vue
@@ -1,31 +1,48 @@
 <template>
-  <div>
-    <div v-for="element in data" >{{ element.title}}</div>
-    
+  <div id="products">
+    <div v-for="element in data" id="single_product">
+      <img :src="element.images[0]" :alt="element.title" srcset="" />
+      <p>{{ element.title }}</p>
+      <p>{{ element.price }}</p>
+    </div>
   </div>
 </template>
 <script setup>
 import axios from "axios";
 import { onMounted } from "vue";
-import {ref} from "vue";
+import { ref } from "vue";
 
-let data=ref([]);
+const data = ref([]);
 
-const getData=()=>{
-    axios.get("https://dummyjson.com/products")
-    .then((res)=>{
-        // console.log(res)
-        data.value=res.data.products;
-        // console.log(data)
+const getData = () => {
+  axios
+    .get("https://dummyjson.com/products")
+    .then((res) => {
+      //   console.log(res)
+      data.value = res.data.products;
+      console.log(data);
     })
-    .catch((err)=>console.log(err))
-}
+    .catch((err) => console.log(err));
+};
 
-onMounted(()=>{
-    getData()
-})
-
-
+onMounted(() => {
+  getData();
+});
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+#products {
+    /* margin-top: 3%; */
+    display: grid;
+    width: 90%;
+    margin: auto;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    grid-gap: 3%;
+}
+#single_product{
+    border: 1px solid red;
+}
+#products>div img{
+    width: 100%;
+}
+</style>
