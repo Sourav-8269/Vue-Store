4f930218dd569c320a7bf187ccc5f332d3b27d0b
Sourav <souravsahu70047@gmail.com>
2023-05-23 21:18:46 +0530

    Added Files

--- vue-project/src/views/Admin.vue
+++ vue-project/src/views/Admin.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
     <h1>This is Admin Page</h1>
+    <input type="text" v-model="form.name" >
+    <h2>{{ form }}</h2>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref,reactive } from 'vue';
+
+const form=reactive({
+  name:""
+});
+
+</script>
 <style scoped></style>
