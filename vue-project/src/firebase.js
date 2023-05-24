26dc12ae1ba491b7618d7edc24e029e383ea60e3
Sourav <souravsahu70047@gmail.com>
2023-05-24 22:33:59 +0530

    Added Firebase Setup

---
+++ vue-project/src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAqn-sOD9qM138BevYgq0XS8KEgoKCdXWU",
+    authDomain: "vuestore-91c09.firebaseapp.com",
+    projectId: "vuestore-91c09",
+    storageBucket: "vuestore-91c09.appspot.com",
+    messagingSenderId: "384433477270",
+    appId: "1:384433477270:web:ae7bb6c025f501769754b7"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export {app,db,collection,getDocs};
