39f9e90502a4f2bc54534c9adee48ec3fa6c0a01
Sourav <souravsahu70047@gmail.com>
2023-06-29 17:43:47 +0530

    Added Files

--- vue-project/src/views/OrderSummary.vue
+++ vue-project/src/views/OrderSummary.vue
@@ -1,34 +1,110 @@
 <template>
-    <div>
-        <h1>Order Summary</h1>
-        <div id="orders">
-            <div id="container" v-for="order in orders">
-      <!-- <h1>This is PD page</h1> -->
-      <div id="lhs">
-        <img
-          src="https://cdn.pixabay.com/photo/2018/11/13/21/43/avatar-3814049_640.png"
-          :alt="order.name"
-          srcset=""
-        />
-      </div>
-      <div id="rhs">
-        <p>Name: {{ order.name }}</p>
-        <p>Address: {{ order.address }}</p>
-        <p>Amount: &#8377; {{ order.total }}</p>
-        <q-btn
-          @click="navigate(order.id)"
-          color="black"
-          label="Show Products"
-        />
-      </div>
-    </div>
+  <div>
+    <h3 id="heading">Order Summary</h3>
+    <div id="orders">
+      <div id="container" v-for="product in cartStore.cart">
+        <!-- {{ products }} -->
+        <div id="lhs">
+          <img :src="product.imageUrl" :alt="product.title" srcset="" />
         </div>
+        <div id="rhs">
+          <p>{{ product.title }}</p>
+          <p>&#8377; {{ product.price }}</p>
+          <div id="quantity-div">
+            <q-icon name="remove" size="30px" id="remove-icon">
+              <q-tooltip class="bg-red" :offset="[10, 10]">Remove</q-tooltip>
+            </q-icon>
+            <p id="quantity">1</p>
+            <q-icon
+              name="add"
+              size="30px"
+              id="remove-icon"
+              @click="removeFromCart(products.id)"
+            >
+              <q-tooltip class="bg-red" :offset="[10, 10]">Add</q-tooltip>
+            </q-icon>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 <script setup>
+import { useCartStore } from "../store";
+import { ref } from "vue";
 
+const cartStore = useCartStore();
 </script>
 
 <style scoped>
-
+#heading {
+  text-align: center;
+  font-size: 30px;
+  margin-top: 10px;
+  /* border: 1px solid red; */
+}
+/* #order{
+    
+} */
+#container {
+  display: flex;
+  padding: 5%;
+  width: 50%;
+  margin: auto;
+  margin-top: 5%;
+  font-size: 180%;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+  border-radius: 8px;
+  justify-content: space-between;
+  /* background-color: #f2f2f2; */
+  margin-bottom: 5%;
+}
+#lhs {
+  display: flex;
+  flex: 5;
+}
+#lhs img {
+  width: 90%;
+}
+#rhs {
+  display: flex;
+  flex: 5;
+  flex-direction: column;
+  margin-left: 5%;
+}
+#quantity-div {
+  display: flex;
+  border: 2px solid #e2e8f0;
+  border-radius: 5px;
+  justify-content: space-around;
+  width: 50%;
+  align-items: center;
+}
+#quantity {
+  font-size: 25px;
+  /* border: 1px solid red; */
+  padding: 0%;
+  margin: 0%;
+}
+@media all and (min-width: 550px) and (max-width: 900px) {
+  #container {
+    width: 70%;
+  }
+  #lhs img {
+    width: 100%;
+  }
+}
+@media all and (min-width: 200px) and (max-width: 550px) {
+  #container {
+    width: 90%;
+    font-size: 20px;
+  }
+  #quantity {
+  font-size: 20px;
+  }
+  #lhs img {
+    width: 100%;
+  }
+}
 </style>
--- vue-project/src/views/Orders.vue
+++ vue-project/src/views/Orders.vue
@@ -38,9 +38,13 @@
               (val) => (val && val.length > 0) || 'Please type something',
             ]"
           />
+          <div id="totalDiv">
+            <p>Total</p>
+            <p>&#8377; {{cartStore.carTotal}}</p>
+          </div>
 
-          <div>
-            <q-btn label="Submit" type="submit" color="primary" />
+          <div id="buttons">
+            <q-btn color="white" text-color="black" label="Cancel" @click="handleNavigate()" />
             <q-btn
               label="Reset"
               type="reset"
@@ -48,21 +52,51 @@
               flat
               class="q-ml-sm"
             />
+            <q-btn label="Submit" type="submit" color="primary" />
           </div>
         </q-form>
       </div>
     </div>
+    <div id="order-summary">
+      <OrderSummaryComponent/>
+    </div>
+    <div class="q-pa-md q-gutter-sm">
+        <q-dialog v-model="seamless" seamless position="bottom">
+          <q-card style="width: 350px">
+            <q-linear-progress :value=band color="green"/>
+
+            <q-card-section class="row items-center no-wrap">
+              <div>
+                <div class="text-weight-bold">Thank You for choosing Vue Store</div>
+                <div class="text-grey">Your Order will be delivered soon!</div>
+              </div>
+
+              <q-space />
+
+              <q-btn flat round icon="close" v-close-popup />
+            </q-card-section>
+          </q-card>
+        </q-dialog>
+      </div>
   </div>
 </template>
 
 <script setup>
+import OrderSummaryComponent from "./OrderSummary.vue";
 import { reactive, ref } from "vue";
 import {useCartStore} from "../store.js";
 import {db, collection,addDoc } from "../firebase.js";
+import { useRouter } from "vue-router";
 
 const cartStore=useCartStore();
+const seamless=ref(false);
+const band=ref(0);
+const router=useRouter();
 
-const file = ref(null);
+const handleNavigate=()=>{
+  router.push("/");
+}
+
 const form = reactive({
   name: "",
   contact: null,
@@ -77,6 +111,10 @@ const onSubmit = async () => {
 
   const docRef=await addDoc(collection(db,"orders"),form);
   console.log(docRef)
+  handleLogin();
+  setTimeout(()=>{
+    handleNavigate();
+  },4000)
 
 };
 
@@ -86,11 +124,22 @@ const onReset = () => {
   form.contact = null;
   form.address = "";
 };
+
+const handleLogin=()=>{
+  seamless.value=true
+  let id=setInterval(()=>{
+    band.value+=0.1;
+    if(band.value>1){
+      clearInterval(id)
+    }
+  },350)
+}
 </script>
 <style scoped>
 *{
   margin: 0;
   padding: 0;
+  font-family: Poppins;
   /* outline: 1px solid red; */
 }
 #heading{
@@ -108,5 +157,22 @@ const onReset = () => {
 .input{
   /* border: 1px solid red; */
   width: 280px;
+}
+#totalDiv{
+  background-color: #2464f0;
+  /* background-color: #f2f2f2; */
+  color: white;
+  padding: 10px;
+  border-radius: 7px;
+  display: flex;
+  justify-content: space-between;
+  font-size: 20px;
+}
+#buttons{
+  /* border: 1px solid red; */
+  display: flex;
+  float: right;
+  gap: 20px;
+  /* justify-content: space-between; */
 }
 </style>
